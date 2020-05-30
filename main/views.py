2d70f8b761aeafba9d4c043b4f4c6c0772eb263e
syu-kwsk <kawasaki.wataru688@mail.kyutech.jp>
2020-05-30 09:50:18 +0900

    Use simple-ace-edit

--- main/views.py
+++ main/views.py
@@ -1,10 +1,14 @@
-from flask import Flask, request, abort
+from flask import Flask, request, abort, redirect, url_for, render_template
 from main import app
 
 @app.route("/")
 def index():
 
     return 'Hello, World'
+
+@app.route("/check")
+def check():
+    return render_template('check.html')
 
 if __name__ == "__main__":
     print("Please run run.py")
