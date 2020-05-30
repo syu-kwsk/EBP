ce85dc6c91f418f3e08380728aa759eac04ab010
syu-kwsk <kawasaki.wataru688@mail.kyutech.jp>
2020-05-30 14:20:35 +0900

    Add layout.html and game.html

--- main/views.py
+++ main/views.py
@@ -1,10 +1,9 @@
-from flask import Flask, request, abort
+from flask import Flask, request, abort, redirect, url_for, render_template
 from main import app
 
 @app.route("/")
-def index():
-
-    return 'Hello, World'
+def game():
+    return render_template('game.html')
 
 if __name__ == "__main__":
     print("Please run run.py")
