941e7408549f0813ecadccc033ec1d5aac59fc4c
shion200 <shion1390@icloud.com>
2020-05-30 22:07:32 +0900

    Create new page

--- main/views.py
+++ main/views.py
@@ -5,5 +5,9 @@ from main import app
 def game():
     return render_template('game.html')
 
+@app.route("/end")
+def finish():
+    return render_template('end.html')
+
 if __name__ == "__main__":
     print("Please run run.py")
