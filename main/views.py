af97a7c399332ac2c4973413b7831541a2c477bf
syu-kwsk <kawasaki.wataru688@mail.kyutech.jp>
2020-05-31 01:24:34 +0900

    Combine game-part and test-part

--- main/views.py
+++ main/views.py
@@ -2,6 +2,10 @@ from flask import Flask, request, abort, redirect, url_for, render_template
 from main import app
 
 @app.route("/")
+def index():
+   return render_template('index.html')
+
+@app.route("/game")
 def game():
     return render_template('game.html')
 
