205928e6a6af339e4fbf6b698427bdc7ec0355d4
syu-kwsk <kawasaki.wataru688@mail.kyutech.jp>
2020-05-30 22:34:47 +0900

    Show result of test code in check.html

--- main/views.py
+++ main/views.py
@@ -6,29 +6,33 @@ def index():
 
     return 'Hello, World'
 
-@app.route("/check", methods=['GET','POST'])
-def check():
+@app.route("/test", methods=['POST'])
+def test():
     if request.method == 'POST':
         import os
         import traceback
+        import subprocess
+
         submitted_script = request.form["submitted_script"]
-        print("Make test.py")
+        #print("Make test.py")
         f = open('test.py','w')
         f.write(submitted_script)
         f.close()
-        print("Save and Run!")
-        print("test.py was finished with following outputs")
-        print("-----------------------")
+        #print("Save and Run!")
+        #print("test.py was finished with following outputs")
+        #print("-----------------------")
         try:
-            import test
-            print("-----------------------")
-            print("Good job!")
+            result = subprocess.check_output(['python', 'test.py']).decode()
         except Exception as e:
-            print(traceback.format_exc())
-            print("-----------------------")
-            print("Never give up!")
+            result= traceback.format_exc()
         os.remove('test.py')
     
+    print(result)
+    session['result'] = result
+    return result
+
+@app.route('/check')
+def check():
     return render_template('check.html')
 
 if __name__ == "__main__":
