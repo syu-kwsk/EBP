4d0daae8f0b760b0a8d798b488b29af28ced3d03
syu-kwsk <kawasaki.wataru688@mail.kyutech.jp>
2020-05-30 14:00:43 +0900

    Check submitted script in flask

--- main/views.py
+++ main/views.py
@@ -9,7 +9,26 @@ def index():
 @app.route("/check", methods=['GET','POST'])
 def check():
     if request.method == 'POST':
-      print(request.form['submitted_script'])
+        import os
+        import traceback
+        submitted_script = request.form["submitted_script"]
+        print("Make test.py")
+        f = open('test.py','w')
+        f.write(submitted_script)
+        f.close()
+        print("Save and Run!")
+        print("test.py was finished with following outputs")
+        print("-----------------------")
+        try:
+            import test
+            print("-----------------------")
+            print("Good job!")
+        except Exception as e:
+            print(traceback.format_exc())
+            print("-----------------------")
+            print("Never give up!")
+        os.remove('test.py')
+    
     return render_template('check.html')
 
 if __name__ == "__main__":
